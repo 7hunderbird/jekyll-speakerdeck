9aaae04ae23a1e4a2d512f2f6500cc5b636e4d2e
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-03-30 23:43:16 +0900

    change option args

--- lib/jekyll/speakerdeck/tag_parser.rb
+++ lib/jekyll/speakerdeck/tag_parser.rb
@@ -1,10 +1,27 @@
+require 'shellwords'
+
 module Jekyll::Speakerdeck
   class TagParser
-    attr_accessor :speakerdeck_id, :data_slide
+    attr_accessor :speakerdeck_id, :options
+
     def initialize(markup)
-      options = markup.split(/,/)
-      @speakerdeck_id = options[0].strip
-      @data_slide = options[1].strip if options[1]
+      @params = markup.shellsplit
+      @speakerdeck_id = @params.shift
+    end
+
+    def options #fix me
+      @options ||= @params.map do |var|
+        key, val = var.split(/=/)
+        %|#{key}="#{val}"| if valid?(key)
+      end.compact.join(" ")
+    end
+
+    def valid?(key)
+      avairable_options.include?(key)
+    end
+
+    def avairable_options
+      ['data-slide']
     end
   end
 end
--- spec/lib/tag_parser_spec.rb
+++ spec/lib/tag_parser_spec.rb
@@ -2,18 +2,31 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 describe Jekyll::Speakerdeck::TagParser do
   let(:speakerdeck_id) { Forgery(:basic).encrypt }
-  let(:data_slide) { Forgery(:basic).number.to_s }
+  let(:options) { %|data-slide="#{Forgery(:basic).number.to_s}"| }
+
   subject { Jekyll::Speakerdeck::TagParser.new(markup) }
 
-  describe "A speakerdeck_id and data_slide." do
-    let(:markup) { "#{speakerdeck_id},#{data_slide}" }
+  describe "A speakerdeck_id and data-slide." do
+    let(:markup) { "#{speakerdeck_id}  #{options}" }
 
     it 'speakerdeck_id must equal speakerdeck_id' do
       subject.speakerdeck_id.must_equal speakerdeck_id
     end
 
-    it 'data_slide must equal data_slide' do
-      subject.data_slide.must_equal data_slide
+    it 'options must equal options' do
+      subject.options.must_equal options
+    end
+  end
+
+  describe "A speakerdeck_id and invalid options." do
+    let(:markup) { %|#{speakerdeck_id}  #{options} with-invalid="options"| }
+
+    it 'speakerdeck_id must equal speakerdeck_id' do
+      subject.speakerdeck_id.must_equal speakerdeck_id
+    end
+
+    it 'options must equal options' do
+      subject.options.must_equal options
     end
   end
 
@@ -24,8 +37,8 @@ describe Jekyll::Speakerdeck::TagParser do
       subject.speakerdeck_id.must_equal speakerdeck_id
     end
 
-    it 'parser.data_slide must be nil' do
-      subject.data_slide.must_be_nil
+    it 'parser.options must be nil' do
+      subject.options.must_be_empty
     end
   end
 
@@ -36,8 +49,8 @@ describe Jekyll::Speakerdeck::TagParser do
       subject.speakerdeck_id.must_equal speakerdeck_id
     end
 
-    it 'data_slide must be nil' do
-      subject.data_slide.must_be_nil
+    it 'options must be nil' do
+      subject.options.must_be_empty
     end
   end
 
@@ -48,8 +61,8 @@ describe Jekyll::Speakerdeck::TagParser do
       subject.speakerdeck_id.must_equal speakerdeck_id
     end
 
-    it 'data_slide must be nil' do
-      subject.data_slide.must_be_nil
+    it 'options must be nil' do
+      subject.options.must_be_empty
     end
   end
 
@@ -60,28 +73,20 @@ describe Jekyll::Speakerdeck::TagParser do
       subject.speakerdeck_id.must_equal speakerdeck_id
     end
 
-    it 'data_slide must be nil' do
-      subject.data_slide.must_be_nil
+    it 'options must be nil' do
+      subject.options.must_be_empty
     end
   end
 
   describe "Space between the elements" do
-    let(:markup) { " #{speakerdeck_id},    #{data_slide}  " }
+    let(:markup) { " #{speakerdeck_id}    #{options}  " }
 
     it 'speakerdeck_id must equal speakerdeck_id' do
       subject.speakerdeck_id.must_equal speakerdeck_id
     end
 
-    it 'data_slide must equal data_slide' do
-      subject.data_slide.must_equal data_slide
-    end
-  end
-
-  describe "Blank markup" do
-    let(:markup) { "" }
-
-    it "Initialize must raise NoMethodError" do
-      proc { subject }.must_raise NoMethodError
+    it 'options must equal options' do
+      subject.options.must_equal options
     end
   end
 end
